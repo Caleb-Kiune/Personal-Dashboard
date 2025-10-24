e11eebdcc8f30393fc3d8dbdf2efb786cfda05e7
Caleb <calebkiune@gmail.com>
2025-10-24 14:26:13 +0300

    adds author and some error handling

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 async function getData() {
-	const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
-	const data = await res.json()
-	document.body.style.backgroundImage = `url('${data.urls.full}')`
-	console.log(data.urls)
+  try {
+    const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    const data = await res.json()
+    document.body.style.backgroundImage = `url('${data.urls.full}')`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+    console.log(data.urls)
+  } catch (err) {
+    document.body.style.backgroundImage = `url('https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1758967905380-f62a8c8fd373')`
+  }
+	
  }
  getData()
