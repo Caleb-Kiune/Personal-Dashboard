f2299a161fb706cbd70f135cc413aa7b45604096
Caleb <calebkiune@gmail.com>
2025-10-25 17:21:31 +0300

    adds weather data and $ to crypto prices

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function getData() {
     const data = await res.json()
     document.body.style.backgroundImage = `url('${data.urls.full}')`
     document.getElementById("author").textContent = `By: ${data.user.name}`
-    console.log(data.urls)
+    // console.log(data.urls)
   } catch (err) {
     document.body.style.backgroundImage = `url('https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1758967905380-f62a8c8fd373')`
   }
@@ -22,14 +22,14 @@ fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
   return res.json()
 })
  .then(data => {
-  console.log(data.name)
-        console.log(data.image)
+  // console.log(data.name)
+        // console.log(data.image)
         document.getElementById('crypto-name').textContent = data.name
         document.getElementById('crypto-image').src = data.image.small
 
-        document.getElementById('crypto-current').textContent += data.market_data.current_price.usd
-        document.getElementById('crypto-high').textContent += data.market_data.high_24h.usd
-        document.getElementById('crypto-low').textContent += data.market_data.low_24h.usd
+        document.getElementById('crypto-current').textContent += `$${data.market_data.current_price.usd}`
+        document.getElementById('crypto-high').textContent += `$${data.market_data.high_24h.usd}`
+        document.getElementById('crypto-low').textContent += `$${data.market_data.low_24h.usd}`
 
 })
  .catch(err => console.log(err))
@@ -41,8 +41,33 @@ fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
     const seconds = new Date().getSeconds()
     const amPm = hours >= 12 ? 'PM' : 'AM'
     const minutesUpdate = minutes < 10 ? `${0}${minutes}` : minutes
+    const secondsUpdate = seconds < 10 ? `${0}${seconds}` : seconds
     const hour12 = hours === 0 ? 12 : hours > 12 ? hours - 12 : hours
-    const currentTime = `${hour12}:${minutesUpdate}:${seconds} ${amPm}`
+    const currentTime = `${hour12}:${minutesUpdate}:${secondsUpdate} ${amPm}`
     document.getElementById('time').textContent = currentTime
-    console.log(currentTime)
+    // console.log(currentTime)
   }, 1000);
+
+
+  //grab current weather data based on location 
+  navigator.geolocation.getCurrentPosition(position => {
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
+    
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`)
+    .then(res => res.json())
+    .then(data => {
+            const weatherIcon = data.weather[0].icon
+            const temperature = Math.round(data.main.temp)
+            const city = data.name
+            // console.log(city)
+            document.getElementById('temp').textContent = temperature
+            document.getElementById('city').textContent = city
+          
+            document.getElementById('weather-image').src = 
+            `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            
+        })
+        .catch(err => console.error(err))
+});
+
